44d675b8e238ba811172b6d2cd86c2e4a806bf11
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-11-09 19:07:46 +0000

    Implement Eq for Vec3i

--- src/math/vector/vec3i.rs
+++ src/math/vector/vec3i.rs
@@ -3,7 +3,7 @@ use rand_derive2::RandGen;
 
 
 #[repr(C)]
-#[derive(Copy, Clone, Debug, PartialEq, RandGen, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, RandGen, Hash)]
 pub struct Vec3i {
 	pub x: i32,
 	pub y: i32,
