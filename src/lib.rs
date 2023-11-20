068bdc305b20696abe6761834cd6f5f0a657ef50
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-11-20 12:35:43 +0000

    Remove old unused cruft

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,7 @@
-#![feature(generators, generator_trait)]
 #![feature(const_fn_floating_point_arithmetic)]
 
-pub mod ordified;
-pub mod mut_rc; 
 pub mod math;
-pub mod coro;
 
 pub use rand;
 
-pub use ordified::*;
-pub use mut_rc::*;
 pub use math::*;
-pub use coro::*;
-
-#[macro_export]
-#[deprecated(note="use matches! instead")]
-macro_rules! match_pattern {
-	($v:expr, $p:pat) => {
-		match $v {
-			$p => true,
-			_ => false,
-		}
-	}
-}
