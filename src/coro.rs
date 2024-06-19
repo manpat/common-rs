80a69c465555c83a6bc3784880fbb342b2ded76b
Patrick Monaghan <0x5f.manpat@gmail.com>
2024-06-19 17:32:19 +0100

    fixes for maintaining old web cruft

--- src/coro.rs
+++ src/coro.rs
@@ -1,16 +1,16 @@
-use std::ops::{Generator, GeneratorState};
+use std::ops::{Coroutine, CoroutineState};
 use std::pin::Pin;
 
 pub struct Coro<Y> {
 	valid: bool,
-	coro: Pin<Box<dyn Generator<Yield=Y, Return=()>>>,
+	coro: Pin<Box<dyn Coroutine<Yield=Y, Return=()>>>,
 }
 
 impl<Y> Coro<Y> {
 	pub fn is_valid(&self) -> bool { self.valid }
 }
 
-impl<Y, G> From<G> for Coro<Y> where G: 'static + Generator<Yield=Y, Return=()> {
+impl<Y, G> From<G> for Coro<Y> where G: 'static + Coroutine<Yield=Y, Return=()> {
 	fn from(gen: G) -> Self {
 		Coro {
 			coro: Box::pin(gen),
@@ -24,7 +24,7 @@ impl<Y> Iterator for Coro<Y> {
 	fn next(&mut self) -> Option<Self::Item> {
 		if !self.valid { return None }
 
-		if let GeneratorState::Yielded(yielded_value) = Pin::new(&mut self.coro).resume(()) {
+		if let CoroutineState::Yielded(yielded_value) = Pin::new(&mut self.coro).resume(()) {
 			Some(yielded_value)
 		} else {
 			self.valid = false;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(generators, generator_trait)]
+#![feature(coroutines, coroutine_trait)]
 #![feature(const_fn_floating_point_arithmetic)]
 
 pub mod ordified;
